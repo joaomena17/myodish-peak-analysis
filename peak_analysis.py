67bc73245e204c57bfa9b5ff12b27660c6fa5d7a
JOUV <jouv>
2024-07-05 09:57:23 +0000

    peak analysis summary statistics working

--- peak_analysis.py
+++ peak_analysis.py
@@ -6,9 +6,11 @@ class PeakAnalysis:
     def __init__(self, data: PeakDetection):
         self.data = data
         self.peak_objects = []
-        self.compute_forces()
+        self.ttp_points = []
+        self.ttr_points = []
+        self.compute_attributtes()
 
-    def compute_forces(self):
+    def compute_attributtes(self):
         peaks_idx, peaks_timestamp, peaks_val = self.data.detect_peaks()
 
         peaks =  zip(peaks_idx, zip(peaks_timestamp, peaks_val))
@@ -27,24 +29,54 @@ class PeakAnalysis:
             metadata['force'] = peak_val - baseline[peak_idx]
 
             # Calculate time to peak and time to relaxation
-            i = peak_idx
+            i = 0
 
-            while self.data.smooth[i] != diastolic_reference_at_peaks[i] and i > 0:
-                i-=1
+            if peak_idx == 0 or peak_idx >= len(self.data.smooth) - 2 or peak_idx >= len(diastolic_reference_at_peaks) - 2:
+                metadata['time_to_peak'] = np.nan
+                metadata['time_to_relaxation'] = np.nan
+                metadata['duration'] = np.nan
+                self.peak_objects.append(metadata)
+                continue
+
+            ttp_edge_reference: bool = self.data.smooth[peak_idx - i] >= diastolic_reference_at_peaks[peak_idx - i]
+            ttp_new_edge_reference =  not ttp_edge_reference
+
+            while ttp_edge_reference != ttp_new_edge_reference and peak_idx - i > 0:
+                # update i
+                i+=1
+
+                # update reference
+                ttp_edge_reference = self.data.smooth[peak_idx - i] >= diastolic_reference_at_peaks[peak_idx - i]
 
             if i == 0:
                 metadata['time_to_peak'] = np.nan
             else:
-                metadata['time_to_peak'] = self.data.data[self.data.time_column][peak_idx] - self.data.data[self.data.time_column][i]
+                metadata['time_to_peak'] = (i)/100 # seconds
+                self.ttp_points.append((peak_timestamp - i/100, self.data.smooth[peak_idx - i]))
 
-            i = peak_idx
-            while self.data.smooth[i] != diastolic_reference_at_peaks[i] and i < len(self.data.smooth) - 1:
+            i = 0
+            ttr_edge_reference = self.data.smooth[peak_idx + i] <= diastolic_reference_at_peaks[peak_idx + i]
+            ttr_new_edge_reference =  not ttr_edge_reference
+
+            while ttr_edge_reference != ttr_new_edge_reference and peak_idx + i < len(self.data.smooth) - 1:
+                # update i
                 i+=1
+
+                # update reference
+                ttr_edge_reference = self.data.smooth[peak_idx + i] <= diastolic_reference_at_peaks[peak_idx + i]
             
-            if i == len(self.data.smooth) - 1:
+            if peak_idx + i == len(self.data.smooth) - 1:
                 metadata['time_to_relaxation'] = np.nan
             else:
-                metadata['time_to_relaxation'] = self.data.data[self.data.time_column][i] - self.data.data[self.data.time_column][peak_idx]
+                metadata['time_to_relaxation'] = (i)/100 # seconds
+                self.ttr_points.append((peak_timestamp + i/100, self.data.smooth[peak_idx + i]))
+    
+
+            # Calculate total duration of peak
+            if not np.isnan(metadata['time_to_peak']) and not np.isnan(metadata['time_to_relaxation']):
+                metadata['duration'] = metadata['time_to_relaxation'] + metadata['time_to_peak']
+            else:
+                metadata['duration'] = np.nan
 
             self.peak_objects.append(metadata)
 
