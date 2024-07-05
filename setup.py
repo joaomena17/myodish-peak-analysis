fd2fecf806d2e30696cbc82900c531e94e30998a
JOUV <jouv>
2024-07-05 13:37:40 +0000

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="myodish-peak-analysis",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
